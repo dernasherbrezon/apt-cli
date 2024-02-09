c07ffe4fd1ea43c8b7ee3cf15ecf3bd2f2b5aca1
dernasherbrezon <dernasherbrezon@gmail.com>
2024-02-09 20:05:41 +0000

    fail on unsupported format

--- src/main/java/ru/r2cloud/apt/cli/Main.java
+++ src/main/java/ru/r2cloud/apt/cli/Main.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.compress.utils.IOUtils;
 import org.slf4j.Logger;
@@ -42,116 +43,123 @@ import ru.r2cloud.apt.model.SignConfiguration;
 
 public class Main {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] argv) {
-        CleanupCommand cleanup = new CleanupCommand();
-        DeleteCommand delete = new DeleteCommand();
-        SaveCommand save = new SaveCommand();
-        InitCommand init = new InitCommand();
-        CommandLineArgs args = new CommandLineArgs();
-        JCommander parser = JCommander.newBuilder().addObject(args).addCommand("init", init).addCommand("save", save).addCommand("cleanup", cleanup).addCommand("delete", delete).build();
-        try {
-            parser.parse(argv);
-        } catch (ParameterException e) {
-            LOG.error(e.getMessage());
-            parser.usage();
-            System.exit(-1);
-        }
-        if (args.isHelp()) {
-            parser.usage();
-            return;
-        }
+	public static void main(String[] argv) {
+		CleanupCommand cleanup = new CleanupCommand();
+		DeleteCommand delete = new DeleteCommand();
+		SaveCommand save = new SaveCommand();
+		InitCommand init = new InitCommand();
+		CommandLineArgs args = new CommandLineArgs();
+		JCommander parser = JCommander.newBuilder().addObject(args).addCommand("init", init).addCommand("save", save).addCommand("cleanup", cleanup).addCommand("delete", delete).build();
+		try {
+			parser.parse(argv);
+		} catch (ParameterException e) {
+			LOG.error(e.getMessage());
+			parser.usage();
+			System.exit(-1);
+		}
+		if (args.isHelp()) {
+			parser.usage();
+			return;
+		}
 
-        URI uri = null;
-        try {
-            uri = new URI(args.getUrl());
-        } catch (URISyntaxException e) {
-            LOG.error("invalid url specified: {}", args.getUrl(), e);
-            System.exit(-1);
-            return;
-        }
+		URI uri = null;
+		try {
+			uri = new URI(args.getUrl());
+		} catch (URISyntaxException e) {
+			LOG.error("invalid url specified: {}", args.getUrl(), e);
+			System.exit(-1);
+			return;
+		}
 
-        Transport transport = null;
-        if (uri.getScheme().equalsIgnoreCase("s3")) {
-            String path = uri.getPath();
-            if (path.length() > 1 && (path.charAt(0) == '/')) {
-                path = path.substring(1);
-            }
-            transport = new AwsTransport(uri.getHost(), args.getAwsRegion(), path, args.getTimeout(), null);
-        } else if (uri.getScheme().equalsIgnoreCase("file")) {
-            String basedirPath = uri.getAuthority() + uri.getPath();
-            try {
-                transport = new FileTransport(basedirPath);
-            } catch (IOException e) {
-                LOG.error("invalid file path specified: {}", basedirPath, e);
-                System.exit(-1);
-                return;
-            }
-        } else {
-            // scheme should be validated by jcommander above
-            System.exit(-1);
-            return;
-        }
+		Transport transport = null;
+		if (uri.getScheme().equalsIgnoreCase("s3")) {
+			String path = uri.getPath();
+			if (path.length() > 1 && (path.charAt(0) == '/')) {
+				path = path.substring(1);
+			}
+			transport = new AwsTransport(uri.getHost(), args.getAwsRegion(), path, args.getTimeout(), null);
+		} else if (uri.getScheme().equalsIgnoreCase("file")) {
+			String basedirPath = uri.getAuthority() + uri.getPath();
+			try {
+				transport = new FileTransport(basedirPath);
+			} catch (IOException e) {
+				LOG.error("invalid file path specified: {}", basedirPath, e);
+				System.exit(-1);
+				return;
+			}
+		} else {
+			// scheme should be validated by jcommander above
+			System.exit(-1);
+			return;
+		}
 
-        GpgSigner signer = null;
-        if (args.getKeyname() != null) {
-            SignConfiguration config = new SignConfiguration();
-            config.setKeyname(args.getKeyname());
-            config.setGpgCommand(args.getGpgExecutable());
-            if (args.getPassphraseFile() != null) {
-                try (InputStream is = new BufferedInputStream(new FileInputStream(args.getPassphraseFile()))) {
-                    config.setPassphrase(new String(IOUtils.toByteArray(is), StandardCharsets.UTF_8).trim());
-                } catch (IOException e) {
-                    LOG.error("unable to read passphrase file: {}", args.getPassphraseFile(), e);
-                    System.exit(-1);
-                    return;
-                }
-            }
-            if (args.getGpgArguments() != null) {
-                config.setGpgArguments(args.getGpgArguments());
-            }
-            signer = new GpgSignerImpl(config);
-        }
+		GpgSigner signer = null;
+		if (args.getKeyname() != null) {
+			SignConfiguration config = new SignConfiguration();
+			config.setKeyname(args.getKeyname());
+			config.setGpgCommand(args.getGpgExecutable());
+			if (args.getPassphraseFile() != null) {
+				try (InputStream is = new BufferedInputStream(new FileInputStream(args.getPassphraseFile()))) {
+					config.setPassphrase(new String(IOUtils.toByteArray(is), StandardCharsets.UTF_8).trim());
+				} catch (IOException e) {
+					LOG.error("unable to read passphrase file: {}", args.getPassphraseFile(), e);
+					System.exit(-1);
+					return;
+				}
+			}
+			if (args.getGpgArguments() != null) {
+				config.setGpgArguments(args.getGpgArguments());
+			}
+			signer = new GpgSignerImpl(config);
+		}
 
-        AptRepository aptMan = new AptRepositoryImpl(args.getCodename(), args.getComponent(), signer, transport);
-        try {
-            if (parser.getParsedCommand().equals("save")) {
-                List<DebFile> files = new ArrayList<>();
-                for (String curPattern : save.getPatterns()) {
-                    @SuppressWarnings("resource")
-                    PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + curPattern);
-                    try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(new File(".").toPath(), matcher::matches)) {
-                        dirStream.forEach(path -> {
-                            try {
-                                files.add(new DebFile(path.toFile()));
-                            } catch (Exception e) {
-                                LOG.info("skipping: {} not a deb file", path, e);
-                            }
-                        });
-                    }
-                }
-                aptMan.saveFiles(files);
-            } else if (parser.getParsedCommand().equals("init")) {
-                Architecture[] architectures = new Architecture[init.getArchs().size()];
-                for (int i = 0; i < init.getArchs().size(); i++) {
-                    architectures[i] = Architecture.valueOf(init.getArchs().get(i).toUpperCase(Locale.UK));
-                }
-                aptMan.init(architectures);
-            } else if (parser.getParsedCommand().equals("cleanup")) {
-                aptMan.cleanup(cleanup.getKeepLast());
-            } else if (parser.getParsedCommand().equals("delete")) {
-                aptMan.deletePackages(new HashSet<>(delete.getPackages()));
-            } else {
-                LOG.error("unknown command: {}", parser.getParsedCommand());
-                parser.usage();
-                System.exit(-1);
-            }
-        } catch (IOException e) {
-            LOG.error("unable to run command: {}", parser.getParsedCommand(), e);
-            System.exit(-1);
-        }
+		AptRepository aptMan = new AptRepositoryImpl(args.getCodename(), args.getComponent(), signer, transport);
+		try {
+			if (parser.getParsedCommand().equals("save")) {
+				List<DebFile> files = new ArrayList<>();
+				AtomicInteger foundFiles = new AtomicInteger(0);
+				for (String curPattern : save.getPatterns()) {
+					@SuppressWarnings("resource")
+					PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + curPattern);
+					try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(new File(".").toPath(), matcher::matches)) {
+						dirStream.forEach(path -> {
+							try {
+								foundFiles.incrementAndGet();
+								files.add(new DebFile(path.toFile()));
+							} catch (Exception e) {
+								LOG.info("skipping: {} not a deb file", path, e);
+							}
+						});
+					}
+				}
+				// most likely invalid/unsupported format of .deb files
+				// fail in that case
+				if (foundFiles.get() > 0 && files.isEmpty()) {
+					System.exit(-1);
+				}
+				aptMan.saveFiles(files);
+			} else if (parser.getParsedCommand().equals("init")) {
+				Architecture[] architectures = new Architecture[init.getArchs().size()];
+				for (int i = 0; i < init.getArchs().size(); i++) {
+					architectures[i] = Architecture.valueOf(init.getArchs().get(i).toUpperCase(Locale.UK));
+				}
+				aptMan.init(architectures);
+			} else if (parser.getParsedCommand().equals("cleanup")) {
+				aptMan.cleanup(cleanup.getKeepLast());
+			} else if (parser.getParsedCommand().equals("delete")) {
+				aptMan.deletePackages(new HashSet<>(delete.getPackages()));
+			} else {
+				LOG.error("unknown command: {}", parser.getParsedCommand());
+				parser.usage();
+				System.exit(-1);
+			}
+		} catch (IOException e) {
+			LOG.error("unable to run command: {}", parser.getParsedCommand(), e);
+			System.exit(-1);
+		}
 
-    }
+	}
 
 }
